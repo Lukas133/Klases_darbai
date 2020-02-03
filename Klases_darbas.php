ba70170e60a4a462e86052b02d5dac5eab952dbe
Lukas133 <fnak159@gmail.com>
2020-02-03 11:14:29 +0200

    antra uzduotis

--- Klases_darbas.php
+++ Klases_darbas.php
@@ -1,8 +1,8 @@
 <?php
-// ekrane pavaizduoti teksta ir data sios dienos
-$string = 'Sios dienos data - ';
-$data = date('Y/m/d');
+// sukurti sakini is 5< zodziu. po kiekvieno zodzio yra padetas taskas. jums reikia naudoti pazines php funcijas tuos taskus pakeisti i tarpus;
+$tekstas = 'labas.kaip.sekasi.tau.ka?';
+$labas = str_replace(".", " " ,"$tekstas");
+print "$tekstas <br>";
+print $labas;
 
-$rezultatas = $string . $data;
-print $rezultatas;
  ?>
