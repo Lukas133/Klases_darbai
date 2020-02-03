7cde0090376773d5ebf2e38a8c80abdc7d5005de
Lukas133 <fnak159@gmail.com>
2020-02-03 10:46:38 +0200

    pirma uzduotis

--- Klases_darbas.php
+++ Klases_darbas.php
@@ -1,4 +1,8 @@
 <?php
-print 'labukas';
+// ekrane pavaizduoti teksta ir data sios dienos
+$string = 'Sios dienos data - ';
+$data = date('Y/m/d');
 
+$rezultatas = $string . $data;
+print $rezultatas;
  ?>
