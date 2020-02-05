f3bf514b1d323fcb2ce47c022a09ef2e92b1580a
Lukas133 <fnak159@gmail.com>
2020-02-05 09:19:32 +0200

    Klases darbas antra uzduotis

--- Klases_darbas.php
+++ Klases_darbas.php
@@ -1,7 +1,10 @@
 <?php
-$a = 'du';
-$b = 'mano';
-$c = 'batai buvo';
-print $b .' '. $c .' '. $a;
+$a = 5;
+$b = 2;
+if ( $a>$b) {
+print 'a daugiau uz b';
+}else {
+ print 'a maziau uz b arba a lygu b';
+}
 
  ?>
