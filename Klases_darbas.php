dbbe0d59b47357ab1370b9953e51f160732ba002
Lukas133 <fnak159@gmail.com>
2020-02-05 09:05:44 +0200

    Klases darbas pirma uzduotis

--- Klases_darbas.php
+++ Klases_darbas.php
@@ -1,75 +1,7 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+$a = 'du';
+$b = 'mano';
+$c = 'batai buvo';
+print $b .' '. $c .' '. $a;
 
-$cars =
-[
-[
-  'brand' => 'audi',
-  'model' => 'a4',
-  'year' => '2002',
-  'cost' => '1000',
-  'country' => [
-    'Lithuania'=>[
-      'kaunas',
-      'vilnius',
-      'klaipeda',
-    ],
-    'Russia',
-    'Latvia',
-  ]
-],
-  [
-    'brand' => 'bmw',
-    'model' => 'm5',
-    'year' => '2004',
-    'cost' => '18000',
-    'country' => [
-      'Lithuania'=>[
-        'kaunas',
-        'vilnius',
-        'klaipeda',
-      ],
-      'Belgium',
-      'Germany',
-    ]
-  ],
-    [
-      'brand' => 'fiat',
-      'model' => 'panda',
-      'year' => '1996',
-      'cost' => '1000',
-      'country' => [
-        'Lithuania'=>[
-          'kaunas',
-          'vilnius',
-          'klaipeda',
-        ],
-        'Belgium',
-        'Germany',
-      ]
-    ],
-      [
-        'brand' => 'volvo',
-        'model' => 's60',
-        'year' => '2015',
-        'cost' => '27000',
-        'country' => [
-          'Lithuania'=>[
-            'kaunas',
-            'vilnius',
-            'klaipeda',
-          ],
-          'Belgium',
-          'Germany',
-        ]
-        ]
-];
-var_dump($cars);
-
-
-foreach ($cars as $key => $value) {
- var_dump($value['country']['Lithuania']);
-}
  ?>
