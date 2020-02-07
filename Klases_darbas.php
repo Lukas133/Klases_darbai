9e898341885803997f6541c1b7f5eff808279b29
Lukas133 <fnak159@gmail.com>
2020-02-07 15:05:56 +0200

    alaus dezes

--- Klases_darbas.php
+++ Klases_darbas.php
@@ -14,24 +14,17 @@
      <title>Hello, world!</title>
    </head>
    <body class="container mt-5">
-     <form class="" action="Klases_darbas.php" method="POST">
-       <h3>Vardas</h3>
-       <input type="text" name="name" value="">
-       <br>
-       <h3>Emailas</h3>
-       <input type="email" name="email" value="">
-       <br>
-       <button type="submit">submit</button>
-     </form>
-     <table class="table">
-       <?php foreach ($array as $user) :?>
-         <tr>
-           <?php foreach ($user as $info): ?>
-       	     <td><?php print $info; ?> </td>
-           <?php endforeach; ?>
-          </tr>
-        <?php endforeach; ?>
-     </table>
+  <section>
+    <?php for ($i=0; $i < $deziukiekis; $i++) :?>
+      <img src="<?php  print 'https://lh3.googleusercontent.com/proxy/I8gewkoQeQZ3QDvYx02sbTjvIeq-uMD4kB3P8fdk5iXp-OBPtD0-t-Ajx4PgZAGOxHjdYIeh8pqBf0413symp5gSC4HNmljELiWBdXY4AP5nziTdviExBgh7HXKNSvM' ?>" alt="">
+    <?php endfor; ?>
+    <div class="">
+      <?php for ($i=0; $i < $buteliukukiekis; $i++) :?>
+        <img class="img"src="<?php print 'https://www.seekpng.com/png/small/160-1604885_corona-extra-corona-extra-beer-12-fl-oz.png'?>" alt="">
+      <?php endfor; ?>
+    </div>
+
+  </section>
      <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
      <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
--- app/php/php.php
+++ app/php/php.php
@@ -1,21 +1,33 @@
 <?php
 
-//failo prijungimas
-$file = "tekstas.txt";
-$fo = fopen($file, 'a') or die("can't open file");
+$deze = 24;
 
+$penktadienis =rand(4,6);
+$sestadienis =rand(1,2);
 
-fwrite($fo, implode(", ",$_POST) . ", \n");
+$kiekisgeriau = 0;
+$kiekisgeriaupermenesi = 0;
+for ($persavaite=1; $persavaite <= 4 ; $persavaite++) {
+	for ($i=1; $i <= 4 ; $i++) {
+		$darbodienos = rand(1,3);
+//		print 'per diena ' . $darbodienos . "<br>";
+		$kiekisgeriau += $darbodienos;
+	};
+	$penktadienis =rand(4,6);
+	$sestadienis =rand(1,2);
+//	print $penktadienis .' penktadienis' . "<br>";
+//	print $sestadienis .' sestadienis'. "<br>";
+	$kiekisgeriaupersavaite = $kiekisgeriau+$penktadienis+$sestadienis;
+	//print $kiekisgeriaupersavaite . ' per pirma savaite' . "<br>";
 
-fclose($fo);
-
-$file1 = file($file);
-
-$array = [];
-foreach ($file1 as $user) {
-	$naujas = explode(', ', $user);
-	array_pop($naujas);
-	array_push($array, $naujas);
+ $kiekisgeriaupermenesi+=$kiekisgeriaupersavaite;
+	$kiekisgeriau =0;
 }
+print $kiekisgeriaupermenesi ."<br>";
 
- ?>
+$buteliukai = $kiekisgeriaupermenesi/24;
+$deziukiekis = floor($buteliukai);
+print $deziukiekis . " --- deziu skaicius" . "<br>";
+$buteliukukiekis = $kiekisgeriaupermenesi-24*$deziukiekis;
+print $buteliukukiekis . " ---likes buteliuku skaicius";
+	?>
