34d4e12a5d8fb43e6a898dac62b6b5539d8824df
Lukas133 <fnak159@gmail.com>
2020-02-07 11:20:14 +0200

    prisijungimai su kaupiamais duomenimis su txt file

--- Klases_darbas.php
+++ Klases_darbas.php
@@ -14,8 +14,24 @@
      <title>Hello, world!</title>
    </head>
    <body class="container mt-5">
-      
-
+     <form class="" action="Klases_darbas.php" method="POST">
+       <h3>Vardas</h3>
+       <input type="text" name="name" value="">
+       <br>
+       <h3>Emailas</h3>
+       <input type="email" name="email" value="">
+       <br>
+       <button type="submit">submit</button>
+     </form>
+     <table class="table">
+       <?php foreach ($array as $user) :?>
+         <tr>
+           <?php foreach ($user as $info): ?>
+       	     <td><?php print $info; ?> </td>
+           <?php endforeach; ?>
+          </tr>
+        <?php endforeach; ?>
+     </table>
      <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
      <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
